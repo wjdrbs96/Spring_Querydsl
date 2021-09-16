f1cb74cf60a2fc2c9587703dcf91dfd944e578ee
Gyunny <wjdrbs966@naver.com>
2021-09-16 11:58:14 +0900

    initial commit

---
+++ src/main/java/com/qurydsl/QurydslApplication.java
@@ -0,0 +1,13 @@
+package com.qurydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QurydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QurydslApplication.class, args);
+    }
+
+}
